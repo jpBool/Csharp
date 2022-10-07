6581f8c68929bafeb0590e3a4025b61c6ee16117
eiidm <luisa.eid@unesp.br>
2022-10-07 10:44:01 -0300

    coomit

--- Desktop/ProjetoFinal/ProjetoFinal/login.Designer.cs
+++ Desktop/ProjetoFinal/ProjetoFinal/login.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(570, 358);
             this.Name = "login";
             this.Text = "login";
