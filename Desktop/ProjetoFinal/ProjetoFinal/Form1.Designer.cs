95bbd2ee28d67affc291c1fe097b558f096a4220
biancamainini <bianca.mainini@unesp.br>
2022-10-07 10:26:33 -0300

    ibhb

--- Desktop/ProjetoFinal/ProjetoFinal/Form1.Designer.cs
+++ Desktop/ProjetoFinal/ProjetoFinal/Form1.Designer.cs
@@ -37,10 +37,10 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Sienna;
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(-4, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(820, 150);
             this.panel1.TabIndex = 0;
@@ -50,7 +50,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::ProjetoFinal.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(13, 17);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(119, 103);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -75,7 +75,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
