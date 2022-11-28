7b9bcca5aeb069a50ea16d98131cf02378048d8c
jpBool <106327899+jpBool@users.noreply.github.com>
2022-11-28 17:27:21 -0300

    a
    ds

---
+++ Desktop/ProjetoFinal2/ProjetoFinal2/frmProdutos.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ProjetoFinal2
+{
+    partial class frmProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.txtBoxPesquisa = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(61, 21);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.Size = new System.Drawing.Size(336, 150);
+            this.dgvProdutos.TabIndex = 0;
+            // 
+            // txtBoxPesquisa
+            // 
+            this.txtBoxPesquisa.Location = new System.Drawing.Point(61, 205);
+            this.txtBoxPesquisa.Name = "txtBoxPesquisa";
+            this.txtBoxPesquisa.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPesquisa.TabIndex = 1;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(180, 205);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 2;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click_1);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(322, 253);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 3;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(233, 253);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(61, 253);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 5;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(146, 253);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 6;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // frmProdutos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 317);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtBoxPesquisa);
+            this.Controls.Add(this.dgvProdutos);
+            this.Name = "frmProdutos";
+            this.Text = "frmProdutos";
+            
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.TextBox txtBoxPesquisa;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnInserir;
+    }
+}
---
+++ Desktop/ProjetoFinal2/ProjetoFinal2/frmProdutos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql; 
+
+namespace ProjetoFinal2
+{
+    public partial class frmProdutos : Form
+    {
+        private NpgsqlConnection cn = new NpgsqlConnection();
+        private string stringConexao = "server = pgsql.projetoscti.com.br;" +
+                "database = projetoscti; port=5432; " +
+                "user id = projetoscti; password = gaspar";
+
+        public frmProdutos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click_1(object sender, EventArgs e)
+        {
+            //se usuario digitou id 
+            string sql;
+            //sql = "select * from cine order by nome_fantasia";
+            sql = "select * from livros";
+
+            // Criar e configurar um objeto Command - responsável por
+            // processar comandos SQL contra a fonte de dados
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, cn);
+
+            // Criar um DataAdapter do comando sql contra o banco
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+
+            // Carregar os dados do DataAdapter para o DataSet
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            dgvProdutos.DataSource = ds.Tables[0];
+        }
+    }
+}
+
